08891012306c8cdd8a0a2d41b6ef72edb3a9f9b5
ahmet1407 <59695777+ahmet1407@users.noreply.github.com>
2025-06-08 17:14:57 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,9 @@
-# utils.py placeholder
+def detect_platform(url):
+    if "hepsiburada.com" in url:
+        return "hepsiburada"
+    elif "trendyol.com" in url:
+        return "trendyol"
+    elif "amazon.com.tr" in url:
+        return "amazon"
+    else:
+        return "unknown"
